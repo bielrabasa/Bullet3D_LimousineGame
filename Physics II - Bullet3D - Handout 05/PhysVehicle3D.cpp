a01bc33c2afe3f9c42d7e6923420d69099ef568d
Biel Rabasa <bielrabasa@gmail.com>
2021-12-27 16:21:35 +0100

    Handout Push

---
+++ Physics II - Bullet3D - Handout 05/PhysVehicle3D.cpp
@@ -0,0 +1,104 @@
+#include "PhysVehicle3D.h"
+#include "Primitive.h"
+#include "Bullet/include/btBulletDynamicsCommon.h"
+
+// ----------------------------------------------------------------------------
+VehicleInfo::~VehicleInfo()
+{
+	//if(wheels != NULL)
+		//delete wheels;
+}
+
+// ----------------------------------------------------------------------------
+PhysVehicle3D::PhysVehicle3D(btRigidBody* body, btRaycastVehicle* vehicle, const VehicleInfo& info) : PhysBody3D(body), vehicle(vehicle), info(info)
+{
+}
+
+// ----------------------------------------------------------------------------
+PhysVehicle3D::~PhysVehicle3D()
+{
+	delete vehicle;
+}
+
+// ----------------------------------------------------------------------------
+void PhysVehicle3D::Render()
+{
+	Cylinder wheel;
+
+	wheel.color = Blue;
+
+	for(int i = 0; i < vehicle->getNumWheels(); ++i)
+	{
+		wheel.radius = info.wheels[0].radius;
+		wheel.height = info.wheels[0].width;
+
+		vehicle->updateWheelTransform(i);
+		vehicle->getWheelInfo(i).m_worldTransform.getOpenGLMatrix(&wheel.transform);
+
+		wheel.Render();
+	}
+
+	Cube chassis(info.chassis_size.x, info.chassis_size.y/2, info.chassis_size.z);
+	Cube chassis2(info.chassis_size.x, info.chassis_size.y-1, info.chassis_size.z/2);
+	vehicle->getChassisWorldTransform().getOpenGLMatrix(&chassis.transform);
+	vehicle->getChassisWorldTransform().getOpenGLMatrix(&chassis2.transform);
+	btQuaternion q = vehicle->getChassisWorldTransform().getRotation();
+	btVector3 offset(info.chassis_offset.x, info.chassis_offset.y, info.chassis_offset.z);
+	offset = offset.rotate(q.getAxis(), q.getAngle());
+
+	chassis.transform.M[12] += offset.getX();
+	chassis.transform.M[13] += offset.getY()-1;
+	chassis.transform.M[14] += offset.getZ();
+
+	chassis2.transform.M[12] += offset.getX();
+	chassis2.transform.M[13] += offset.getY();
+	chassis2.transform.M[14] += offset.getZ();
+
+	chassis.color = Color(0, 100, 0);
+	chassis2.color = Color(0, 100, 0);
+
+	chassis.Render();
+	chassis2.Render();
+}
+
+// ----------------------------------------------------------------------------
+void PhysVehicle3D::ApplyEngineForce(float force)
+{
+	for(int i = 0; i < vehicle->getNumWheels(); ++i)
+	{
+		if(info.wheels[i].drive == true)
+		{
+			vehicle->applyEngineForce(force, i);
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+void PhysVehicle3D::Brake(float force)
+{
+	for(int i = 0; i < vehicle->getNumWheels(); ++i)
+	{
+		if(info.wheels[i].brake == true)
+		{
+			vehicle->setBrake(force, i);
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+void PhysVehicle3D::Turn(float degrees)
+{
+	for(int i = 0; i < vehicle->getNumWheels(); ++i)
+	{
+		if(info.wheels[i].steering == true)
+		{
+			vehicle->setSteeringValue(degrees, i);
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
+float PhysVehicle3D::GetKmh() const
+{
+	return vehicle->getCurrentSpeedKmHour();
+}
